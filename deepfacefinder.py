8b665f3e4d04235cbe7563a6bddc294a2508e928
tvgh <yankangche@gmail.com>
2023-05-12 00:40:51 +0800

    Update deepfacefinder.py

--- deepfacefinder.py
+++ deepfacefinder.py
@@ -1,14 +1,19 @@
-
 import os
 import cv2
 import shutil
 import threading
 from collections import defaultdict
-from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import Pool
 from deepface import DeepFace
 
-def calculate_distances(img1, imgsfolder, model, img_count, target_img_count, total_images):
-    img1 = cv2.imread(img1)
+def calculate_distances(args):
+    img1path, imgsfolder, model, img_count, target_img_count, total_images = args
+    try:
+        img1 = cv2.imread(img1path)
+    except Exception as e:
+        print(f"Error occurred while reading {img1path}: {str(e)}")
+        return []
+
     distance_scores = []
     count = 0
     for root, _, files in os.walk(imgsfolder):
@@ -18,21 +23,17 @@ def calculate_distances(img1, imgsfolder, model, img_count, target_img_count, to
                     img_path = os.path.join(root, filename)
                     img2 = cv2.imread(img_path)
                     distance_score = DeepFace.verify(img1, img2, model_name=model)["distance"]
-                    distance_scores.append((img_path, distance_score))
-                    count += 1
-                    print(f"Processing target image {img_count}/{target_img_count}, image {count}/{total_images}, distance: {distance_score}")
+                    if distance_score != 0:
+                        distance_scores.append((img_path, distance_score))
+                        count += 1
+                        print(f"Processing target image {img_count}/{target_img_count}, image {count}/{total_images}, distance: {distance_score}")
                 except Exception as e:
-                    print("Error occurred while processing " + filename + ": " + str(e))
+                    print(f"Error occurred while processing {filename}: {str(e)}")
 
                 if count == 50000:
                     break
-    return distance_scores
 
-def process_target_image(img1path, imgsfolder, model, img_count, target_img_count, total_images, sum_distance_scores, lock):
-    distance_scores = calculate_distances(img1path, imgsfolder, model, img_count, target_img_count, total_images)
-    with lock:  
-        for img_path, distance_score in distance_scores:
-            sum_distance_scores[img_path] += distance_score
+    return distance_scores
 
 def main():
     target_folder = "realimage"
@@ -47,7 +48,7 @@ def main():
 
     total_images = sum([len(files) for r, d, files in os.walk(imgsfolder) if any(fname.endswith('.jpg') or fname.endswith('.png') for fname in files)])
 
-    sum_distance_scores = defaultdict(float)
+    avg_distance_scores = defaultdict(float)
     target_img_count = 0
     for root, _, files in os.walk(target_folder):
         target_img_count += sum(1 for filename in files if filename.endswith('.jpg') or filename.endswith('.png'))
@@ -61,15 +62,15 @@ def main():
                 img1path = os.path.join(root, filename)
                 target_images.append((img1path, img_count))
 
-    lock = threading.Lock()
+    with Pool(processes=global_parameter) as pool:
+        results = pool.map(calculate_distances, [(img1path, imgsfolder, model, img_count, target_img_count, total_images) for img1path, img_count in target_images])
+        for result in results:
+            for img_path, distance_score in result:
+                avg_distance_scores[img_path] += distance_score / target_img_count
 
-    with ThreadPoolExecutor(max_workers=global_parameter) as executor:
-        futures = [executor.submit(process_target_image, img1path, imgsfolder, model, img_count, target_img_count, total_images, sum_distance_scores, lock) for img1path, img_count in target_images]
-        _ = [future.result() for future in futures]
+    sorted_avg_distance_scores = sorted(avg_distance_scores.items(), key=lambda x: x[1])
 
-    sorted_sum_distance_scores = sorted(sum_distance_scores.items(), key=lambda x: x[1])
-
-    for i, (img_path, _) in enumerate(sorted_sum_distance_scores[:n_best_matches]):
+    for i, (img_path, _) in enumerate(sorted_avg_distance_scores[:n_best_matches]):
         new_filename = f"z000{i+1}.png"
         shutil.copy2(img_path, os.path.join(destination_folder, new_filename))
 
